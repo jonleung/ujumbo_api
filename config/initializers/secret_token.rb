a541167a98a626fd861873a8c00e1b776f559843
Jonathan Leung <naruto137@gmail.com>
2012-11-08 15:43:13 -0500

    Created Ujumbo API with rails-api gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UjumboApi::Application.config.secret_token = '991be5107bef2d2e17ae0dab97c73340f415eb8906259ccc80ca1762a0e2f1822686be516f8d494d9ac7c107e2fb677835cdc662fa519c883d443dd97ac44331'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+UjumboApi::Application.config.session_store :cookie_store, key: '_ujumbo_api_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# UjumboApi::Application.config.session_store :active_record_store
