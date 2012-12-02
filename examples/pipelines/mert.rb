2c2e025352bc304ac3c4c3f5e9037a0565c5a55c
Jonathan Leung <naruto137@gmail.com>
2012-12-02 13:10:48 -0500

    cleaned up empty files for the master branch

--- examples/pipelines/mert.rb
+++
@@ -1,8 +0,0 @@
-product = Product.new
-product.name = "Mert Scheduling App"
-
-product.users.sync("users", GoogleDoc.new({name: Name, email: Email, phone: Phone}))
-product.contexts.sync("GoogleDoc.new())
-
-pipeline = product.pipelines.new
-pipeline = 
