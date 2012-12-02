4fe46fb0c206034ba3df0607877158e9df57b461
Jonathan Leung <naruto137@gmail.com>
2012-12-02 13:26:38 -0500

    fixed broken route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 UjumboApi::Application.routes.draw do
-  root :to => redirect("https://github.com/jonleung/ujumbo_api")
-  match '/api' => 'api_controller#' 
+  # root :to => redirect("https://github.com/jonleung/ujumbo_api")
+  # match '/api' => 'api_controller#' 
 end
