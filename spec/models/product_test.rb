34945e8e6a7a156d8143e3fed15631fc0abc03f6
Jonathan Leung <naruto137@gmail.com>
2013-01-14 15:40:29 -0500

    Added an outline for another test for Google Docs

--- spec/models/product_test.rb
+++ spec/models/product_test.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
+
+# describe "google docs" do
+#   google_doc_pipe = GoogleDoc.new {
+#     :action => "read"
+#     :spreadsheet => "https://docs.google.com/spreadsheet/ccc?key=0AggpMwEqzvVzdF9LamJhTzZiUU9oN2xxcFFKOTE3VHc#gid=0"
+#     :name => "thing_name"
+#   }
+
+#   user_pipe = UserPipe.new({ #TODO, get this from the Trigger part of the hash
+#                 :action => UserPipe::ACTIONS[:create],
+#                 :platform_properties_list => [:first_name, :last_name, :email, :phone, :role], 
+#                 :product_properties_schema => {
+#                   pennkey: String.to_s,
+#                   password: String.to_s 
+#                 },
+#                 :default_properties => {
+#                   :role => "student" #TODO, hmmm better solution?
+#                 }
+#               }
+#   )
+
+#   google_doc_pipe = GoogleDoc.new {
+#     :spreadsheet => "https://docs.google.com/spreadsheet/ccc?key=0AggpMwEqzvVzdF9LamJhTzZiUU9oN2xxcFFKOTE3VHc#gid=0"
+#     :name => "thing_name"
+#     :action => "write_back"
+#   }
+
+
+# }
+
+#   api_call_trigger_id = pipeline.create_trigger(product.id, "api_call", {role: "student"})
+# end
+
 describe "product" do
 
   it "user pipe and template pipe is working" do
@@ -13,15 +46,15 @@ describe "product" do
 
     pipe_order = []
     # User Pipe
-    user_pipe = UserPipe.new({
+    user_pipe = UserPipe.new({ #TODO, get this from the Trigger part of the hash
                   :action => UserPipe::ACTIONS[:create],
-                  :platform_properties_list => [:first_name, :last_name, :email, :phone, :role],
+                  :platform_properties_list => [:first_name, :last_name, :email, :phone, :role], 
                   :product_properties_schema => {
                     pennkey: String.to_s,
                     password: String.to_s 
                   },
                   :default_properties => {
-                    :role => "student"
+                    :role => "student" #TODO, hmmm better solution?
                   }
                 }
     )
@@ -48,7 +81,7 @@ describe "product" do
                 :action => NotificationPipe::ACTIONS[:create],
                 :type => Notification::TYPES[:sms],
                 :pipelined_references => {
-                  :user_id => "Users:#{user_pipe.id}", #TODO: This is all you should have to specify, this is a bit smarter or more standardize
+                  :user_id => "Users:#{user_pipe.id}:id", #TODO: This is all you should have to specify, this is a bit smarter or more standardize
                   :body => "Templates:#{template_pipe.id}:text" #TODO: you should not have to specify this, just the template id, and it should know what to look for, I guess instead of specifying text, you could specify a template that knows to look for text                  
                 }    
                })
@@ -61,7 +94,7 @@ describe "product" do
     pipeline.save
 
     user_create_trigger_id = pipeline.create_trigger(product.id, "database:user:create", {role: "student"}) #TODO, declare channels somewhere so you won't try to set channels that don't exist
-    api_call_trigger_id = pipeline.create_trigger(product.id, "api_call", {role: "student"})
+    api_call_trigger_id = pipeline.create_trigger(product.id, "api_call", {role: "student"})    
 
     client = ApiClient.new
     student_hash = {
