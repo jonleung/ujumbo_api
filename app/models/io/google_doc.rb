9bdea3e750077164d26720d13fd01b8cc5bf92da
wellecks <wellecks@gmail.com>
2013-04-15 20:38:44 -0400

    removed itself from google_doc

--- app/models/io/google_doc.rb
+++ app/models/io/google_doc.rb
@@ -112,15 +112,7 @@ class GoogleDoc
 
 		key = @file_obj.key
 		self.write_attribute(:url, @file_obj.human_url)
-		self.write_attribute(:url, @file_obj.human_url)
-		itself = GoogleDoc.find(self.id)
-		itself.url = @file_obj.human_url
-		itself.save
-
-		puts "file: #{@file_obj.human_url}"
-		puts "self: #{self.url}"
-
-		debugger
+		
 		if key.length == 23
 			self.write_attribute(:gdoc_key, Base64.encode64(key)[0...-2])
 		else
